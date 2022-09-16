05dc8a677a3a05caf138846e5545a7631ef4809f
sung_uk <103974953+OhYesLee@users.noreply.github.com>
2022-09-16 09:37:54 +0900

    20220916

--- Chapter1/src/ch09/LocalVariableType.java
+++ Chapter1/src/ch09/LocalVariableType.java
@@ -2,17 +2,17 @@ package ch09;
 
 public class LocalVariableType {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		var i = 10;
-		var j = 10.0;
-		var str = "hello";
-		
-		System.out.println(i);
-		System.out.println(j);
-		System.out.println(str);
-		
-		str = "test";
-		//str = 3; 
-	}
+        var i = 10;
+        var j = 10.0;
+        var str = "hello";
+
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(str);
+
+        str = "test";
+        //str = 3;
+    }
 }
